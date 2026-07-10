b9e79307c3b4783d7751cae3e211a13743e2989f
Shamik Bera <berashamik115@gmail.com>
2026-07-10 22:57:47 +0530

    feat(major): updated main to use lifecycle to init roadmap graph and add roadmap router;

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from app.pipelines.roadmap.graph import build_roadmap_graph
+from app.routes.roadmap_router import router as roadmap_router
 
-from app.config import settings
+from app.config import dev_settings as settings
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.roadmap_graph = build_roadmap_graph()
+    yield
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
+    lifespan=lifespan
 )
 
 app.add_middleware(
@@ -16,6 +25,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+app.include_router(roadmap_router, prefix="/api")
 
 @app.get("/health")
 async def health():
